6505390de197d549963700c97f9bf260f8a6e483
vishalbiswas <vshlbiswas@ymail.com>
2017-05-14 09:11:04 +0530

    remove accidentally committed sql project

--- splash/dbo/Tables/attachments.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[attachments] (
-    [attachid] BIGINT          IDENTITY (1, 1) NOT NULL,
-    [image]    VARBINARY (MAX) DEFAULT (NULL) NULL,
-    [size]     BIGINT          DEFAULT ((0)) NULL,
-    PRIMARY KEY CLUSTERED ([attachid] ASC)
-);
-
